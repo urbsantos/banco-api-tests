ebd4297d4c907e76203152287faba9e1639da9f5
Urbano <urbanolopes@gmail.com>
2026-02-06 11:55:00 -0300

    Adicionando fixtures para gerir os corpos das requisicoes

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const { expect } = require("chai");
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
@@ -12,33 +13,27 @@ describe('Transferencias', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async() => {
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 11
             
             const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 11,
-                token: ""
-                })
+            .send(bodyTransferencias)
 
             expect(resposta.status).to.equal(201);                
         })
 
         it('Deve retornar 422 quando o valor da transferencia for abaixo a R$ 10,00', async() => {
+            const bodyTransferencias = { ...postTransferencias}
+            bodyTransferencias.valor = 7
             
             const resposta = await request('http://localhost:3000')
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 9,
-                token: ""
-                })
+            .send(bodyTransferencias)
 
             expect(resposta.status).to.equal(422);
         })
